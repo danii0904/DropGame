153bdc2464385a29b122775b1f20b8942a7b176c
Dani <danielnupa0904@gmail.com>
2021-01-18 15:51:58 +0100

    drop game

---
+++ android/src/com/mygdx/drops_game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.drops_game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.drops_game.mainDrops;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new mainDrops(), config);
+	}
+}
